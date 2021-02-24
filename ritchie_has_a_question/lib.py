f150b0d550d1dbf83cfbe76c49b9821abd5ae37c
Richard van den Broek <vandenbroek.rp@gmail.com>
2021-02-24 11:19:30 -0300

    extra

--- ritchie_has_a_question/lib.py
+++ ritchie_has_a_question/lib.py
@@ -1,7 +1,7 @@
 # ritchie_has_a_question/lib.py
 
 def try_me():
-    return "Hey how are the project preparations going? \nHas everyone seen my question on Slack?"
+    return "Hey how are the project preparations going? Has everyone seen my yesterdays question on Slack?"
 
 if __name__ == "__main__":
     print(try_me())
